1cdc1d581b75320f271db30cad863e4ff96929d6
mdshakilbai603 <mdshakilbai603@gmail.com>
2026-05-16 07:57:45 +0600

    Update server.py

--- server.py
+++ server.py
@@ -35,6 +35,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Render-এর পোর্টের সাথে মিল রেখে রান করা
+    # Render-এর পোর্টের সাথে মিল রেখে রান করা (Port Binding সমস্যা দূর করবে)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
